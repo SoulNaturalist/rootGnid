9b990e1fbb39f05cafd7d490c9604be1c236654e
SoulNaturalist <omg12121@mail.ru>
2023-01-21 10:55:28 +0300

    created basic function to get pc screenshot

---
+++ index.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"image/png"
+	"os"
+
+	"github.com/kbinani/screenshot"
+)
+
+func createScreenShots() {
+	n := screenshot.NumActiveDisplays()
+
+	for i := 0; i < n; i++ {
+		bounds := screenshot.GetDisplayBounds(i)
+
+		img, err := screenshot.CaptureRect(bounds)
+		if err != nil {
+			panic(err)
+		}
+		fileName := fmt.Sprintf("%d_%dx%d.png", i, bounds.Dx(), bounds.Dy())
+		file, _ := os.Create(fileName)
+		defer file.Close()
+		png.Encode(file, img)
+
+		fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
+	}
+}
+
+func main() {
+	createScreenShots()
+
+}
