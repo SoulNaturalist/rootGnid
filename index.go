88b9a92dd27d2b5251562cae06c652c872eb0ba1
SoulNaturalist <58743402+SoulNaturalist@users.noreply.github.com>
2023-01-21 14:48:29 +0300

    Update index.go

--- index.go
+++ index.go
@@ -1,7 +1,57 @@
 package main
 
-func main() {
-	u.createScreenShots()
-	getPcConfig()
+import (
+	"fmt"
+	"image/png"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
 
+	"strings"
+
+	"github.com/kbinani/screenshot"
+	"github.com/pbnjay/memory"
+)
+
+func getPcConfig() {
+
+	m := memory.TotalMemory() / 1000 * 100
+	fmt.Print(strconv.Itoa(int(m)))
+
+	response, error := http.Get("https://api.ipify.org?format=json")
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	body, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+		fmt.Println(error)
+	}
+	response.Body.Close()
+	ip := strings.Replace(strings.Replace(strings.Split(string(body), ":")[1], "}", "", -1), `"`, "", -1)
+	fmt.Printf("\n" + ip)
+}
+
+func createScreenShots() {
+	n := screenshot.NumActiveDisplays()
+
+	for i := 0; i < n; i++ {
+		bounds := screenshot.GetDisplayBounds(i)
+
+		img, err := screenshot.CaptureRect(bounds)
+		if err != nil {
+			panic(err)
+		}
+		fileName := fmt.Sprintf("%d_%dx%d.png", i, bounds.Dx(), bounds.Dy())
+		file, _ := os.Create(fileName)
+		defer file.Close()
+		png.Encode(file, img)
+
+		fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
+	}
+}
+
+func main() {
+	getPcConfig()
 }
